d61812f01d1fae032e87c41390626a3a3aa2e110
Alex <apeggs2000@users.noreply.github.com>
2014-04-05 15:34:08 -0700

    Adds jQuery for video

--- GameMenu.js
+++ GameMenu.js
@@ -1,1 +1,16 @@
- 
+var introScene = document.getElementById('intoScene');
+$(document).ready(function() {
+    //hide all objects except the intro video
+    $('#menu').hide();
+    $('#game').hide();
+    $('#about').hide();
+    $('#how2play').hide();
+    
+    //when the user clicks the video, it will stop and the menu will show
+    $('#introScene').onClick(function() {
+        $(this).hide();
+        $('#menu').show();
+        //introScene.pause();
+    });
+});
+
